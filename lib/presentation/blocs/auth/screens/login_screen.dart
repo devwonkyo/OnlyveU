9671edd0356a3b7618089bc677da0d6d0efa518b
[APP1] 정원교 <dnjsry159@naver.com>
2024-10-28 16:25:19 +0900

    push develop branch

--- lib/presentation/blocs/auth/screens/login_screen.dart
+++ lib/presentation/blocs/auth/screens/login_screen.dart
@@ -10,6 +10,6 @@ class LoginScreen extends StatefulWidget {
 class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
-    return Center(child: const Text("onlve you"));
+    return Center(child: const Text("onlve you!"));
   }
 }
