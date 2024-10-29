813f7df5e2f91ec0040694325b9d88325083ac3a
[APP1] 국한 <kook1488@naver.com>
2024-10-29 10:01:38 +0900

    go 라우터로 바텀바 연결

--- lib/core/router.dart
+++ lib/core/router.dart
@@ -1,13 +1,36 @@
+// lib/config/routes/app_router.dart
 import 'package:go_router/go_router.dart';
+import 'package:onlyveyou/presentation/blocs/home/screens/bottom_navbar.dart';
 import 'package:onlyveyou/presentation/blocs/home/screens/home_screen.dart';
+import 'package:onlyveyou/presentation/screens/category_screen.dart';
+import 'package:onlyveyou/presentation/screens/histoy_screen.dart';
+import 'package:onlyveyou/presentation/screens/my_screen.dart';
 
 final GoRouter router = GoRouter(
   initialLocation: '/home',
   routes: [
-    // Auth 관련 Route
-    GoRoute(
-      path: '/home',
-      builder: (context, state) => Home(),
+    ShellRoute(
+      builder: (context, state, child) {
+        return ScaffoldWithBottomNavBar(child: child);
+      },
+      routes: [
+        GoRoute(
+          path: '/category',
+          builder: (context, state) => CategoryScreen(),
+        ),
+        GoRoute(
+          path: '/home',
+          builder: (context, state) => Home(),
+        ),
+        GoRoute(
+          path: '/history',
+          builder: (context, state) => HistoryScreen(),
+        ),
+        GoRoute(
+          path: '/my',
+          builder: (context, state) => MyScreen(),
+        ),
+      ],
     ),
   ],
 );
