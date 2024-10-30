471c44c1321123c3540ae5f32ce7eb3d3359eb28
tjddus <jb5767sy123@gmail.com>
2024-10-30 16:15:12 +0900

    마이페이지 ui add, 프로필 이미지 설정 add

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:onlyveyou/blocs/mypage/profile_edit/profile_edit_bloc.dart';
 
 import 'core/router.dart';
 import 'firebase_options.dart';
@@ -14,18 +15,24 @@ main() async {
   runApp(const MyApp());
 }
 
-
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      theme: ThemeData(
-        fontFamily: "Pretendard",
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => ProfileEditBloc(),
+        ),
+      ],
+      child: MaterialApp.router(
+        theme: ThemeData(
+          fontFamily: "Pretendard",
+        ),
+        routerConfig: router,
       ),
-      routerConfig: router,
     );
   }
 }
