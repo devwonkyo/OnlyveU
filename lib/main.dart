5757e0ccb06f607de65392995f37eb9227aec113
[APP1] 이영인 <zeroinlee@gmail.com>
2024-10-30 20:42:28 +0900

    검색 화면에 페이지 전환 효과 추가 및 main 함수 반환 타입 수정

--- lib/main.dart
+++ lib/main.dart
@@ -6,20 +6,13 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:onlyveyou/blocs/home/home_bloc.dart';
 import 'package:onlyveyou/blocs/mypage/profile_edit/profile_edit_bloc.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:onlyveyou/cubit/category/category_cubit.dart';
 
 import 'blocs/history/history_bloc.dart';
 import 'core/router.dart';
 import 'firebase_options.dart';
 
-<<<<<<< HEAD
 void main() async {
-=======
-
-main() async {
->>>>>>> 3110bd2f8261be842a2945e55e9f37350493554b
   // Flutter 바인딩 초기화 (반드시 필요)
   WidgetsFlutterBinding.ensureInitialized();
 
