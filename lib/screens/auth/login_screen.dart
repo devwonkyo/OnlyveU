3ccc30bca82a543dff17c3595e606d37430b3bc6
SweetFriedPotato <123169135+SweetFriedPotato@users.noreply.github.com>
2024-10-31 17:59:41 +0900

    로그인, 회원가입, (이메일), sharedpreference

--- lib/screens/auth/login_screen.dart
+++ lib/screens/auth/login_screen.dart
@@ -1,20 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
-import 'package:onlyveyou/core/router.dart';
+import 'package:onlyveyou/blocs/auth/auth_bloc.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:onlyveyou/blocs/auth/auth_event.dart';
+import 'package:onlyveyou/blocs/auth/auth_state.dart';
+import 'package:onlyveyou/utils/shared_preference_util.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-
-void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp.router(
-      routerConfig: router,
-    );
-  }
-}
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class LoginScreen extends StatefulWidget {
   @override
@@ -22,185 +14,160 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
-  bool _autoLogin = false;
-  bool _saveId = false;
-  TextEditingController _idController = TextEditingController();
-  TextEditingController _passwordController = TextEditingController();
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+
+  void _showDialog(String message) {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          content: Text(message),
+          actions: [
+            TextButton(
+              onPressed: () => Navigator.of(context).pop(),
+              child: Text('확인'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  Future<void> _saveUserInfoToSharedPrefs(String userId) async {
+    try {
+      // Firebase에서 유저 정보 가져오기
+      DocumentSnapshot userDoc = await FirebaseFirestore.instance
+          .collection('users')
+          .doc(userId)
+          .get();
+
+      if (userDoc.exists) {
+        // Firebase에서 가져온 데이터
+        String email = userDoc.get('email');
+        String gender = userDoc.get('gender');
+        String nickname = userDoc.get('nickname');
+        String phone = userDoc.get('phone');
+
+        // SharedPreferences에 저장
+        await OnlyYouSharedPreference().setEmail(email);
+        await OnlyYouSharedPreference().setGender(gender);
+        await OnlyYouSharedPreference().setNickname(nickname);
+        await OnlyYouSharedPreference().setPhone(phone);
+      }
+    } catch (e) {
+      print("유저 정보 저장 오류: $e");
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        elevation: 0,
-        backgroundColor: Colors.white,
-        leading: IconButton(
-          icon: Icon(Icons.close, color: Colors.black),
-          onPressed: () {
-            Navigator.pop(context);
-          },
+    return BlocListener<AuthBloc, AuthState>(
+      listener: (context, state) {
+        if (state is AuthLoading) {
+          // 로딩 다이얼로그 표시
+          showDialog(
+            context: context,
+            barrierDismissible: false,
+            builder: (context) => Center(child: CircularProgressIndicator()),
+          );
+        } else if (state is LoginSuccess) {
+          Navigator.of(context).pop(); // 로딩 다이얼로그 닫기
+          _saveUserInfoToSharedPrefs(state.userId); // 로그인 성공 시 유저 정보 저장
+          context.go('/home'); // 홈 화면으로 이동
+        } else if (state is AuthFailure) {
+          Navigator.of(context).pop(); // 로딩 다이얼로그 닫기
+          _showDialog(state.message); // 실패 메시지 표시
+        }
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          elevation: 0,
+          backgroundColor: Colors.white,
+          leading: IconButton(
+            icon: Icon(Icons.close, color: Colors.black),
+            onPressed: () {
+              context.pop(); // 이전 화면으로 돌아가기
+            },
+          ),
         ),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            SizedBox(height: 20),
-            Center(
-              child: Column(
-                children: [
-                  Text(
-                    'ONLY`ve YOU',
+        body: GestureDetector(
+          onTap: () => FocusScope.of(context).unfocus(),
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                SizedBox(height: 20),
+                Center(
+                  child: Text(
+                    '로그인',
                     style: TextStyle(
                       fontSize: 24,
                       fontWeight: FontWeight.bold,
                     ),
                   ),
-                ],
-              ),
-            ),
-            SizedBox(height: 40),
-            TextField(
-              controller: _idController,
-              keyboardType: TextInputType.emailAddress,
-              decoration: InputDecoration(
-                hintText: '이메일 주소',
-                border: OutlineInputBorder(),
-              ),
-            ),
-            SizedBox(height: 10),
-            TextField(
-              controller: _passwordController,
-              obscureText: true,
-              decoration: InputDecoration(
-                hintText: '비밀번호 (8~12자, 영문+숫자)',
-                border: OutlineInputBorder(),
-              ),
-            ),
-            SizedBox(height: 10),
-            Row(
-              children: [
-                Checkbox(
-                  value: _autoLogin,
-                  onChanged: (bool? value) {
-                    setState(() {
-                      _autoLogin = value!;
-                    });
-                  },
                 ),
-                Text('자동로그인'),
-                SizedBox(width: 10),
-                Checkbox(
-                  value: _saveId,
-                  onChanged: (bool? value) {
-                    setState(() {
-                      _saveId = value!;
-                    });
-                  },
+                SizedBox(height: 40),
+                TextField(
+                  controller: emailController,
+                  decoration: InputDecoration(
+                    hintText: '이메일',
+                    border: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.black),
+                    ),
+                  ),
                 ),
-                Text('아이디 저장'),
-              ],
-            ),
-            SizedBox(height: 10),
-            ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.black,
-                padding: EdgeInsets.symmetric(vertical: 15),
-              ),
-              onPressed: _signInWithEmailAndPassword, // 로그인 함수 호출
-              child: Text('로그인', style: TextStyle(color: Colors.white)),
-            ),
-            SizedBox(height: 10),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                TextButton(
+                SizedBox(height: 10),
+                TextField(
+                  controller: passwordController,
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    hintText: '비밀번호',
+                    border: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.black),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 20),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.black,
+                    padding: EdgeInsets.symmetric(vertical: 15),
+                  ),
                   onPressed: () {
-                    context.go('/find-id'); // 아이디 찾기 화면으로 이동
+                    context.read<AuthBloc>().add(LoginRequested(
+                          email: emailController.text,
+                          password: passwordController.text,
+                        ));
                   },
-                  child: Text('아이디 찾기', style: TextStyle(color: Colors.black)),
+                  child: Text('로그인', style: TextStyle(color: Colors.white)),
                 ),
-                Text('|', style: TextStyle(color: Colors.grey)),
-                TextButton(
-                  onPressed: () {
-                    // 비밀번호 찾기 화면으로 이동하는 코드 작성
-                  },
-                  child: Text('비밀번호 찾기', style: TextStyle(color: Colors.black)),
+                Spacer(),
+                Center(
+                  child: Column(
+                    children: [
+                      Text(
+                        '계정이 없으신가요?',
+                        style: TextStyle(fontSize: 12, color: Colors.grey),
+                      ),
+                      TextButton(
+                        onPressed: () {
+                          context.go('/signup'); // 회원가입 화면으로 이동
+                        },
+                        child: Text(
+                          '회원가입',
+                          style: TextStyle(color: Colors.black),
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
               ],
             ),
-            SizedBox(height: 10),
-            ElevatedButton.icon(
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.yellow,
-                padding: EdgeInsets.symmetric(vertical: 15),
-              ),
-              onPressed: () {},
-              icon: Icon(Icons.chat_bubble, color: Colors.black),
-              label: Text('카카오로 로그인', style: TextStyle(color: Colors.black)),
-            ),
-            SizedBox(height: 10),
-            ElevatedButton.icon(
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.white,
-                padding: EdgeInsets.symmetric(vertical: 15),
-                side: BorderSide(color: Colors.grey),
-              ),
-              onPressed: () {},
-              icon: Icon(Icons.g_mobiledata_outlined, color: Colors.black),
-              label: Text('Google로 로그인', style: TextStyle(color: Colors.black)),
-            ),
-            SizedBox(height: 10),
-            OutlinedButton(
-              style: OutlinedButton.styleFrom(
-                padding: EdgeInsets.symmetric(vertical: 15),
-                side: BorderSide(color: Colors.grey),
-              ),
-              onPressed: () {
-                context.go('/signup'); // 회원가입 화면으로 이동
-              },
-              child: Text('회원가입', style: TextStyle(color: Colors.black)),
-            ),
-            Spacer(),
-            Center(
-              child: Column(
-                children: [
-                  Text(
-                    'ver 3.18.0 최신 버전    캐시 데이터 삭제    오픈 라이센스',
-                    style: TextStyle(fontSize: 12, color: Colors.grey),
-                  ),
-                  Text(
-                    '고객센터 · 공지사항',
-                    style: TextStyle(fontSize: 12, color: Colors.grey),
-                  ),
-                ],
-              ),
-            ),
-          ],
+          ),
         ),
       ),
     );
-  }
-
-  Future<void> _signInWithEmailAndPassword() async {
-    try {
-      final credential = await FirebaseAuth.instance.signInWithEmailAndPassword(
-        email: _idController.text,
-        password: _passwordController.text,
-      );
-      // 로그인 성공 시 다음 화면으로 이동 (예: 홈 화면)
-      context.go('/home');
-    } on FirebaseAuthException catch (e) {
-      String message;
-      if (e.code == 'user-not-found') {
-        message = '사용자를 찾을 수 없습니다.';
-      } else if (e.code == 'wrong-password') {
-        message = '비밀번호가 틀렸습니다.';
-      } else {
-        message = '로그인에 실패했습니다.';
-      }
-      ScaffoldMessenger.of(context)
-          .showSnackBar(SnackBar(content: Text(message)));
-    }
   }
 }
