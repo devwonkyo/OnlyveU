66a0652776b3d2aee69b8ca7ba0e535126ba1f39
tjddus <jb5767sy123@gmail.com>
2024-10-31 17:59:02 +0900

    내 정보 수정 Ui 추가, 닉네임, 비밀번호 변경 추가

---
+++ lib/blocs/mypage/nickname_edit/nickname_edit_state.dart
@@ -0,0 +1,38 @@
+import 'package:equatable/equatable.dart';
+
+abstract class NicknameEditState extends Equatable {
+  const NicknameEditState();
+
+  @override
+  List<Object?> get props => [];
+}
+
+// 초기 상태
+class NicknameEditInitial extends NicknameEditState {}
+
+// 닉네임이 입력된 상태
+class NicknameEditing extends NicknameEditState {
+  final String nickname;
+  final bool isButtonEnabled;
+
+  const NicknameEditing(this.nickname, {required this.isButtonEnabled});
+
+  @override
+  List<Object?> get props => [nickname, isButtonEnabled];
+}
+
+// 닉네임 변경 요청 중 상태
+class NicknameEditInProgress extends NicknameEditState {}
+
+// 닉네임 변경 성공 상태
+class NicknameEditSuccess extends NicknameEditState {}
+
+// 닉네임 변경 실패 상태
+class NicknameEditFailure extends NicknameEditState {
+  final String errorMessage;
+
+  const NicknameEditFailure(this.errorMessage);
+
+  @override
+  List<Object?> get props => [errorMessage];
+}
