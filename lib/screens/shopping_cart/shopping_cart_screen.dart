0feb7a0a5eeb2a8ebddff92b4ad0035dcd82af7d
[APP1] 국한 <kook1488@naver.com>
2024-11-02 21:40:50 +0900

    블럭 리펙토링

--- lib/screens/shopping_cart/shopping_cart_screen.dart
+++ lib/screens/shopping_cart/shopping_cart_screen.dart
@@ -1,9 +1,12 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:onlyveyou/models/product_model.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
+import '../../blocs/shopping_cart/shopping_cart_bloc.dart';
 import 'widgets/cart_bottombar_widget.dart';
 import 'widgets/cart_tab_header_widget.dart';
+//스크린에서 1탭헤더. 2바텀바만 쓰고
+//위젯이 1탭헤더에서 프라이스 위젯쓰고, 프로덕트리스트 위젯쓴다.
+///     2바텀바에서 프라이스 위젯 쓴다
 
 class ShoppingCartScreen extends StatefulWidget {
   @override
@@ -13,197 +16,99 @@ class ShoppingCartScreen extends StatefulWidget {
 class _ShoppingCartScreenState extends State<ShoppingCartScreen>
     with SingleTickerProviderStateMixin {
   late TabController _tabController;
-  bool isAllSelected = false;
-  Map<String, bool> selectedItems = {};
-  Map<String, int> itemQuantities = {};
-  // late 제거하고 빈 리스트로 초기화
-  List<ProductModel> regularDeliveryItems = [];
-  List<ProductModel> pickupItems = [];
-
-  // 로딩 상태 추가
-  bool isLoading = true;
 
   @override
   void initState() {
     super.initState();
     _tabController = TabController(length: 2, vsync: this);
-    _loadProducts();
+    context.read<CartBloc>().add(LoadCart());
 
     _tabController.addListener(() {
-      setState(() {
-        isAllSelected = false;
-        selectedItems.clear();
-      });
-    });
-  }
-
-  // 상품 로드 함수 분리
-  Future<void> _loadProducts() async {
-    try {
-      final snapshot = await FirebaseFirestore.instance
-          .collection('products')
-          .limit(5)
-          .get();
-
-      setState(() {
-        regularDeliveryItems = snapshot.docs
-            .map((doc) => ProductModel.fromFirestore(doc))
-            .toList();
-
-        // 초기값 설정
-        for (var item in regularDeliveryItems) {
-          selectedItems[item.productId] = true;
-          itemQuantities[item.productId] = 1;
-        }
-        isLoading = false;
-      });
-    } catch (e) {
-      print('Error fetching products: $e');
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text('상품 정보를 불러오는데 실패했습니다.')),
-      );
-      isLoading = false;
-    }
-  }
-
-  void updateItemSelection(String productId, bool? value) {
-    setState(() {
-      selectedItems[productId] = value ?? false;
-      final currentItems =
-          _tabController.index == 0 ? regularDeliveryItems : pickupItems;
-      isAllSelected =
-          currentItems.every((item) => selectedItems[item.productId] == true);
-    });
-  }
-
-  void moveToRegularDelivery() {
-    setState(() {
-      List<ProductModel> itemsToMove = pickupItems
-          .where((item) => selectedItems[item.productId] == true)
-          .toList();
-      pickupItems.removeWhere((item) => selectedItems[item.productId] == true);
-      regularDeliveryItems.addAll(itemsToMove);
-      selectedItems.clear();
-      isAllSelected = false;
-      _tabController.animateTo(0);
-    });
-  }
-
-  void onSelectAll(bool? value) {
-    setState(() {
-      final currentItems =
-          _tabController.index == 0 ? regularDeliveryItems : pickupItems;
-      for (var item in currentItems) {
-        selectedItems[item.productId] = value ?? false;
-      }
-      isAllSelected = value ?? false;
-    });
-  }
-
-  void onDeleteSelected() {
-    setState(() {
-      if (_tabController.index == 0) {
-        regularDeliveryItems
-            .removeWhere((item) => selectedItems[item.productId] == true);
-      } else {
-        pickupItems
-            .removeWhere((item) => selectedItems[item.productId] == true);
-      }
-      selectedItems.clear();
-      isAllSelected = false;
-    });
-  }
-
-  void moveToPickup() {
-    setState(() {
-      List<ProductModel> itemsToMove = regularDeliveryItems
-          .where((item) => selectedItems[item.productId] == true)
-          .toList();
-      regularDeliveryItems
-          .removeWhere((item) => selectedItems[item.productId] == true);
-      pickupItems.addAll(itemsToMove);
-      selectedItems.clear();
-      isAllSelected = false;
-      _tabController.animateTo(1);
-    });
-  }
-
-  void updateQuantity(String productId, bool increment) {
-    setState(() {
-      int currentQuantity = itemQuantities[productId] ?? 1;
-      if (increment && currentQuantity < 99) {
-        itemQuantities[productId] = currentQuantity + 1;
-      } else if (!increment && currentQuantity > 1) {
-        itemQuantities[productId] = currentQuantity - 1;
-      }
+      final cartBloc = context.read<CartBloc>();
+      cartBloc.add(const SelectAllItems(false));
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    final currentItems =
-        _tabController.index == 0 ? regularDeliveryItems : pickupItems;
-
-    return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(
-        backgroundColor: Colors.white,
-        elevation: 0,
-        leading: IconButton(
-          icon: Icon(Icons.arrow_back, color: Colors.black),
-          onPressed: () => Navigator.pop(context),
-        ),
-        title: Text(
-          '장바구니',
-          style: TextStyle(
-            color: Colors.black,
-            fontSize: 18,
-            fontWeight: FontWeight.w500,
-          ),
-        ),
-        actions: [
-          IconButton(
-            icon: Icon(Icons.search, color: Colors.black),
-            onPressed: () {},
-          ),
-          IconButton(
-            icon: Icon(Icons.home_outlined, color: Colors.black),
-            onPressed: () {},
-          ),
-        ],
-      ),
-      body: isLoading
-          ? Center(child: CircularProgressIndicator())
-          : CartTabHeaderWidget(
-              regularDeliveryItems: regularDeliveryItems,
-              pickupItems: pickupItems,
-              selectedItems: selectedItems,
-              itemQuantities: itemQuantities,
-              isAllSelected: isAllSelected,
-              onSelectAll: onSelectAll,
-              onRemoveItem: (item) => setState(() {
-                if (_tabController.index == 0) {
-                  regularDeliveryItems.remove(item);
-                } else {
-                  pickupItems.remove(item);
-                }
-                selectedItems.remove(item.productId);
-                itemQuantities.remove(item.productId);
-              }),
-              updateQuantity: updateQuantity,
-              onUpdateSelection: updateItemSelection,
-              onDeleteSelected: onDeleteSelected,
-              moveToPickup: moveToPickup,
-              moveToRegularDelivery: moveToRegularDelivery,
-              tabController: _tabController,
+    return BlocBuilder<CartBloc, CartState>(
+      builder: (context, state) {
+        return Scaffold(
+          backgroundColor: Colors.white,
+          appBar: AppBar(
+            backgroundColor: Colors.white,
+            elevation: 0,
+            leading: IconButton(
+              icon: Icon(Icons.arrow_back, color: Colors.black),
+              onPressed: () => Navigator.pop(context),
             ),
-      bottomNavigationBar: isLoading
-          ? null
-          : CartBottomBarWidget(
-              currentItems: currentItems,
-              selectedItems: selectedItems,
-              itemQuantities: itemQuantities,
+            title: Text(
+              '장바구니',
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 18,
+                fontWeight: FontWeight.w500,
+              ),
             ),
+            actions: [
+              IconButton(
+                icon: Icon(Icons.search, color: Colors.black),
+                onPressed: () {},
+              ),
+              IconButton(
+                icon: Icon(Icons.home_outlined, color: Colors.black),
+                onPressed: () {},
+              ),
+            ],
+          ),
+          body: state.isLoading
+              ? Center(child: CircularProgressIndicator())
+              : CartTabHeaderWidget(
+                  regularDeliveryItems: state.regularDeliveryItems,
+                  pickupItems: state.pickupItems,
+                  selectedItems: state.selectedItems,
+                  itemQuantities: state.itemQuantities,
+                  isAllSelected: state.isAllSelected,
+                  onSelectAll: (value) {
+                    context
+                        .read<CartBloc>()
+                        .add(SelectAllItems(value ?? false));
+                  },
+                  onRemoveItem: (item) {
+                    context.read<CartBloc>().add(RemoveItem(item));
+                  },
+                  updateQuantity: (productId, increment) {
+                    context
+                        .read<CartBloc>()
+                        .add(UpdateItemQuantity(productId, increment));
+                  },
+                  onUpdateSelection: (productId, value) {
+                    context
+                        .read<CartBloc>()
+                        .add(UpdateItemSelection(productId, value ?? false));
+                  },
+                  onDeleteSelected: () {
+                    context.read<CartBloc>().add(DeleteSelectedItems());
+                  },
+                  moveToPickup: () {
+                    context.read<CartBloc>().add(MoveToPickup());
+                  },
+                  moveToRegularDelivery: () {
+                    context.read<CartBloc>().add(MoveToRegularDelivery());
+                  },
+                  tabController: _tabController,
+                ),
+          bottomNavigationBar: state.isLoading
+              ? null
+              : CartBottomBarWidget(
+                  currentItems: _tabController.index == 0
+                      ? state.regularDeliveryItems
+                      : state.pickupItems,
+                  selectedItems: state.selectedItems,
+                  itemQuantities: state.itemQuantities,
+                ),
+        );
+      },
     );
   }
 
