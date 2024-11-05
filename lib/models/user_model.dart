c6ee79bc31d389be8ea3869dfa370c19684d5600
SweetFriedPotato <123169135+SweetFriedPotato@users.noreply.github.com>
2024-11-05 12:17:55 +0900

    구글로그인, 파이어베이스에 저장

--- lib/models/user_model.dart
+++ lib/models/user_model.dart
@@ -1,1 +1,48 @@
-//파이어베이스 데이터베이스
+class UserModel {
+  String uid;
+  String email;
+  String nickname;
+  List<String> recentSearches; // 최근 검색어
+  List<String> likedItems; // 좋아요한 리스트
+  List<String> cartItems; // 장바구니 아이템
+  List<String> pickupItems; // 픽업 예약 아이템
+  List<String> viewHistory; // 조회 히스토리
+
+  UserModel({
+    required this.uid,
+    required this.email,
+    required this.nickname,
+    this.recentSearches = const [],
+    this.likedItems = const [],
+    this.cartItems = const [],
+    this.pickupItems = const [],
+    this.viewHistory = const [],
+  });
+
+  // Firebase와 상호작용을 위한 toMap 및 fromMap 메서드도 추가 가능
+  Map<String, dynamic> toMap() {
+    return {
+      'uid': uid,
+      'email': email,
+      'nickname': nickname,
+      'recentSearches': recentSearches,
+      'likedItems': likedItems,
+      'cartItems': cartItems,
+      'pickupItems': pickupItems,
+      'viewHistory': viewHistory,
+    };
+  }
+
+  factory UserModel.fromMap(Map<String, dynamic> map) {
+    return UserModel(
+      uid: map['uid'] ?? '',
+      email: map['email'] ?? '',
+      nickname: map['nickname'] ?? '',
+      recentSearches: List<String>.from(map['recentSearches'] ?? []),
+      likedItems: List<String>.from(map['likedItems'] ?? []),
+      cartItems: List<String>.from(map['cartItems'] ?? []),
+      pickupItems: List<String>.from(map['pickupItems'] ?? []),
+      viewHistory: List<String>.from(map['viewHistory'] ?? []),
+    );
+  }
+}
