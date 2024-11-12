b975d6d01d10b2daefc5bad3ee3fc30dec68ef76
[APP1] 이영인 <nyile4423@gmail.com>
2024-11-12 22:40:21 +0900

    UI 개선: 최근 검색어 및 추천 키워드 섹션의 레이아웃 수정

--- lib/screens/search/search_home_screen/search_home_screen.dart
+++ lib/screens/search/search_home_screen/search_home_screen.dart
@@ -22,68 +22,65 @@ class SearchHomeScreen extends StatelessWidget {
       },
       child: Padding(
         padding: EdgeInsets.only(top: 20.h),
-        child: SingleChildScrollView(
-          child: Column(
-            children: [
-              // 로컬 데이터
-              BlocBuilder<RecentSearchBloc, RecentSearchState>(
-                builder: (context, state) {
-                  print(state);
-                  if (state is RecentSearchInitial) {
-                    return const SizedBox();
-                  } else if (state is RecentSearchLoading) {
-                    return const Center(child: Text('나중에 로딩화면 구현하기'));
-                  } else if (state is RecentSearchLoaded) {
-                    return SearchMainContainer(
-                      title: '최근 검색어',
-                      child: SizedBox(
-                        height: 40.h,
-                        child: RecentSearchView(
-                          itemCount: state.recentSearches.length,
-                          titleList: state.recentSearches,
-                        ),
+        child: Column(
+          children: [
+            // 로컬 데이터
+            BlocBuilder<RecentSearchBloc, RecentSearchState>(
+              builder: (context, state) {
+                print(state);
+                if (state is RecentSearchInitial) {
+                  return const SizedBox();
+                } else if (state is RecentSearchLoading) {
+                  return const Center(child: Text('나중에 로딩화면 구현하기'));
+                } else if (state is RecentSearchLoaded) {
+                  return SearchMainContainer(
+                    title: '최근 검색어',
+                    child: SizedBox(
+                      height: 40.h,
+                      child: RecentSearchView(
+                        itemCount: state.recentSearches.length,
+                        titleList: state.recentSearches,
                       ),
-                    );
-                  } else {
-                    return const SizedBox();
-                  }
-                },
-              ),
-              SizedBox(height: 20.h),
-              SearchMainContainer(
-                title: '추천 키워드',
-                child: Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 20.w),
-                  child: Wrap(
-                    spacing: 10.w,
-                    children: [
-                      FilledButton(
-                          onPressed: () {}, child: const Text('세미매트밀착쿠션')),
-                      FilledButton(
-                          onPressed: () {}, child: const Text('콜라겐올인원')),
-                      FilledButton(
-                          onPressed: () {}, child: const Text('히알루산올인원')),
-                      FilledButton(
-                          onPressed: () {}, child: const Text('탱글젤리블리셔')),
-                    ],
-                  ),
-                ),
-              ),
-              SizedBox(height: 20.h),
-              // 서버 데이터
-              SearchMainContainer(
-                title: '급상승 검색어',
-                child:
-                    // const SizedBox(),
-                    Column(
+                    ),
+                  );
+                } else {
+                  return const SizedBox();
+                }
+              },
+            ),
+            SizedBox(height: 20.h),
+            SearchMainContainer(
+              title: '추천 키워드',
+              child: Padding(
+                padding: EdgeInsets.symmetric(horizontal: 20.w),
+                child: Wrap(
+                  spacing: 10.w,
                   children: [
-                    BrandSuggestionUpdateButton(),
-                    CategorySuggestionUpdateButton(),
+                    FilledButton(
+                        onPressed: () {}, child: const Text('세미매트밀착쿠션')),
+                    FilledButton(onPressed: () {}, child: const Text('콜라겐올인원')),
+                    FilledButton(
+                        onPressed: () {}, child: const Text('히알루산올인원')),
+                    FilledButton(
+                        onPressed: () {}, child: const Text('탱글젤리블리셔')),
                   ],
                 ),
               ),
-            ],
-          ),
+            ),
+            SizedBox(height: 20.h),
+            // 서버 데이터
+            SearchMainContainer(
+              title: '급상승 검색어',
+              child:
+                  // const SizedBox(),
+                  Column(
+                children: [
+                  BrandSuggestionUpdateButton(),
+                  CategorySuggestionUpdateButton(),
+                ],
+              ),
+            ),
+          ],
         ),
       ),
     );
