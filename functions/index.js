b21682d44246e2c611865fa9957be34381033d26
[APP1] 정원교 <dnjsry159@naver.com>
2024-11-18 20:13:50 +0900

    푸쉬 작업중

--- functions/index.js
+++ functions/index.js
@@ -1,13 +1,32 @@
 const functions = require("firebase-functions");
+const admin = require("firebase-admin");
 
-exports.helloWorld = functions.region("asia-northeast3").https.onRequest((request, response) => {
-    response.send("Hello from Firebase!");
+
+admin.initializeApp();
+
+exports.sendPushNotification = functions.region("asia-northeast3").https.onCall(async (data, context) => {
+    // 클라이언트로부터 받은 데이터
+    const {token, title, body, screen} = data;
+    console.log("token:", token, "title:", title, "body:", body);
+    // 푸시 알림 메시지 구성
+    const message = {
+        notification: {
+            title: title,
+            body: body,
+        },
+        data: {
+            screen: screen,
+        },
+        token: token,
+    };
+
+    try {
+        // 푸시 알림 전송
+        const response = await admin.messaging().send(message);
+        console.log("Successfully sent message:", response);
+        return {success: true, message: "Notification sent successfully"};
+    } catch (error) {
+        console.log("Error sending message:", error);
+        throw new functions.https.HttpsError("internal", "Error sending push notification");
+    }
 });
-
-// Create and deploy your first functions
-// https://firebase.google.com/docs/functions/get-started
-
-// exports.helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
