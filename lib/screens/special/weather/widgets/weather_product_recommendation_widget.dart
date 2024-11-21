c46c4150cf302e3938c47d280c1a5dfcecf462dd
[APP1] 국한 <kook1488@naver.com>
2024-11-21 12:58:36 +0900

    페이지 이동 완료

--- lib/screens/special/weather/widgets/weather_product_recommendation_widget.dart
+++ lib/screens/special/weather/widgets/weather_product_recommendation_widget.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:go_router/go_router.dart';
 import 'package:onlyveyou/blocs/special_bloc/weather/weather_bloc.dart';
 import 'package:onlyveyou/models/product_model.dart';
 import 'package:onlyveyou/utils/format_price.dart';
@@ -65,6 +66,7 @@ class WeatherProductRecommendationWidget extends StatelessWidget {
                   mainAxisExtent: 280.h,
                 ),
                 itemBuilder: (context, index) => _buildRecommendationCard(
+                  context,
                   state.recommendedProducts[index],
                 ),
                 itemCount: state.recommendedProducts.length,
@@ -78,174 +80,177 @@ class WeatherProductRecommendationWidget extends StatelessWidget {
     );
   }
 
-  Widget _buildRecommendationCard(ProductModel product) {
-    return Container(
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(16),
-        boxShadow: [
-          BoxShadow(
-            color: Colors.black.withOpacity(0.05),
-            blurRadius: 10,
-            offset: Offset(0, 2),
-          ),
-        ],
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          ClipRRect(
-            borderRadius: BorderRadius.vertical(top: Radius.circular(16)),
-            child: Container(
-              height: 150.h,
-              child: Stack(
-                children: [
-                  if (product.productImageList.isNotEmpty)
-                    Image.network(
-                      product.productImageList.first,
-                      width: double.infinity,
-                      height: 150.h,
-                      fit: BoxFit.cover,
-                      errorBuilder: (context, error, stackTrace) => Center(
+  Widget _buildRecommendationCard(BuildContext context, ProductModel product) {
+    return GestureDetector(
+      onTap: () => context.push('/product-detail', extra: product.productId),
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(16),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withOpacity(0.05),
+              blurRadius: 10,
+              offset: Offset(0, 2),
+            ),
+          ],
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            ClipRRect(
+              borderRadius: BorderRadius.vertical(top: Radius.circular(16)),
+              child: Container(
+                height: 150.h,
+                child: Stack(
+                  children: [
+                    if (product.productImageList.isNotEmpty)
+                      Image.network(
+                        product.productImageList.first,
+                        width: double.infinity,
+                        height: 150.h,
+                        fit: BoxFit.cover,
+                        errorBuilder: (context, error, stackTrace) => Center(
+                          child: Icon(
+                            Icons.image,
+                            color: Colors.grey,
+                            size: 40.sp,
+                          ),
+                        ),
+                      )
+                    else
+                      Center(
                         child: Icon(
                           Icons.image,
                           color: Colors.grey,
                           size: 40.sp,
                         ),
                       ),
-                    )
-                  else
-                    Center(
-                      child: Icon(
-                        Icons.image,
-                        color: Colors.grey,
-                        size: 40.sp,
+                    Positioned(
+                      top: 8,
+                      right: 8,
+                      child: Container(
+                        padding: EdgeInsets.symmetric(
+                          horizontal: 8.w,
+                          vertical: 4.h,
+                        ),
+                        decoration: BoxDecoration(
+                          gradient: LinearGradient(
+                            colors: [Color(0xFFFFA41B), Color(0xFFFF7E5F)],
+                          ),
+                          borderRadius: BorderRadius.circular(12),
+                          boxShadow: [
+                            BoxShadow(
+                              color: Color(0xFFFF7E5F).withOpacity(0.3),
+                              blurRadius: 4,
+                              offset: Offset(0, 2),
+                            ),
+                          ],
+                        ),
+                        child: Row(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            Icon(
+                              Icons.wb_sunny_outlined,
+                              color: Colors.white,
+                              size: 12.sp,
+                            ),
+                            SizedBox(width: 4.w),
+                            Text(
+                              '날씨 맞춤',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 10.sp,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ],
+                        ),
                       ),
                     ),
-                  Positioned(
-                    top: 8,
-                    right: 8,
-                    child: Container(
-                      padding: EdgeInsets.symmetric(
-                        horizontal: 8.w,
-                        vertical: 4.h,
-                      ),
-                      decoration: BoxDecoration(
-                        gradient: LinearGradient(
-                          colors: [Color(0xFFFFA41B), Color(0xFFFF7E5F)],
-                        ),
-                        borderRadius: BorderRadius.circular(12),
-                        boxShadow: [
-                          BoxShadow(
-                            color: Color(0xFFFF7E5F).withOpacity(0.3),
-                            blurRadius: 4,
-                            offset: Offset(0, 2),
+                  ],
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(12.w),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    product.name,
+                    style: TextStyle(
+                      color: Color(0xFF2D3436),
+                      fontSize: 14.sp,
+                      fontWeight: FontWeight.bold,
+                    ),
+                    maxLines: 2,
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                  SizedBox(height: 4.h),
+                  Row(
+                    children: [
+                      if (product.discountPercent > 0)
+                        Container(
+                          padding: EdgeInsets.symmetric(
+                            horizontal: 6.w,
+                            vertical: 2.h,
                           ),
-                        ],
-                      ),
-                      child: Row(
-                        mainAxisSize: MainAxisSize.min,
-                        children: [
-                          Icon(
-                            Icons.wb_sunny_outlined,
-                            color: Colors.white,
-                            size: 12.sp,
+                          decoration: BoxDecoration(
+                            color: Color(0xFFFFF4E3),
+                            borderRadius: BorderRadius.circular(4),
                           ),
-                          SizedBox(width: 4.w),
-                          Text(
-                            '날씨 맞춤',
+                          child: Text(
+                            '${product.discountPercent}%',
                             style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 10.sp,
+                              color: Color(0xFFFFA41B),
+                              fontSize: 12.sp,
                               fontWeight: FontWeight.bold,
                             ),
                           ),
-                        ],
+                        ),
+                      SizedBox(width: 8.w),
+                      Text(
+                        '₩${formatPrice(product.price)}',
+                        style: TextStyle(
+                          color: Color(0xFF2D3436),
+                          fontSize: 14.sp,
+                          fontWeight: FontWeight.bold,
+                        ),
                       ),
-                    ),
+                    ],
+                  ),
+                  SizedBox(height: 4.h),
+                  Row(
+                    children: [
+                      Icon(
+                        Icons.star,
+                        color: Color(0xFFFFD700),
+                        size: 14.sp,
+                      ),
+                      SizedBox(width: 4.w),
+                      Text(
+                        product.rating.toStringAsFixed(1),
+                        style: TextStyle(
+                          color: Color(0xFF7F8C8D),
+                          fontSize: 12.sp,
+                        ),
+                      ),
+                      SizedBox(width: 4.w),
+                      Text(
+                        '(${product.reviewList.length})',
+                        style: TextStyle(
+                          color: Color(0xFF7F8C8D),
+                          fontSize: 12.sp,
+                        ),
+                      ),
+                    ],
                   ),
                 ],
               ),
             ),
-          ),
-          Padding(
-            padding: EdgeInsets.all(12.w),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  product.name,
-                  style: TextStyle(
-                    color: Color(0xFF2D3436),
-                    fontSize: 14.sp,
-                    fontWeight: FontWeight.bold,
-                  ),
-                  maxLines: 2,
-                  overflow: TextOverflow.ellipsis,
-                ),
-                SizedBox(height: 4.h),
-                Row(
-                  children: [
-                    if (product.discountPercent > 0)
-                      Container(
-                        padding: EdgeInsets.symmetric(
-                          horizontal: 6.w,
-                          vertical: 2.h,
-                        ),
-                        decoration: BoxDecoration(
-                          color: Color(0xFFFFF4E3),
-                          borderRadius: BorderRadius.circular(4),
-                        ),
-                        child: Text(
-                          '${product.discountPercent}%',
-                          style: TextStyle(
-                            color: Color(0xFFFFA41B),
-                            fontSize: 12.sp,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                      ),
-                    SizedBox(width: 8.w),
-                    Text(
-                      '₩${formatPrice(product.price)}',
-                      style: TextStyle(
-                        color: Color(0xFF2D3436),
-                        fontSize: 14.sp,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ],
-                ),
-                SizedBox(height: 4.h),
-                Row(
-                  children: [
-                    Icon(
-                      Icons.star,
-                      color: Color(0xFFFFD700),
-                      size: 14.sp,
-                    ),
-                    SizedBox(width: 4.w),
-                    Text(
-                      product.rating.toStringAsFixed(1),
-                      style: TextStyle(
-                        color: Color(0xFF7F8C8D),
-                        fontSize: 12.sp,
-                      ),
-                    ),
-                    SizedBox(width: 4.w),
-                    Text(
-                      '(${product.reviewList.length})',
-                      style: TextStyle(
-                        color: Color(0xFF7F8C8D),
-                        fontSize: 12.sp,
-                      ),
-                    ),
-                  ],
-                ),
-              ],
-            ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
